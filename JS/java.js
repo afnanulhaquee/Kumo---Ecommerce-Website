03e3a176066a9cdc4fa59544b8d70bb15bc5f65a
Afnan Ul Haq <afnanulhaq4@gmail.com>
2024-09-15 18:36:16 +0500

    Complete Site

---
+++ JS/java.js
@@ -0,0 +1,23 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", this.window.scrollY > 0);
+});
+
+let menu = document.querySelector("#menu-icon");
+let navmenu = document.querySelector(".navmenu");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navmenu.classList.toggle("open");
+};
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
